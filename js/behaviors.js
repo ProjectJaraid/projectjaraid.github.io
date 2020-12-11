1fbc24ac5580ae6e14ccb655f14d60e0c9d4b669
Hugh Cayless <philomousos@gmail.com>
2020-12-11 13:00:35 -0500

    Changed chrono.html to fully static page.

--- js/behaviors.js
+++ js/behaviors.js
@@ -12,36 +12,38 @@ var behaviors = {
       rs.appendChild(tooltip);
     },
     "table": function(elt) {
-      let table = document.createElement("table");
-      table.innerHTML = elt.innerHTML;
-      if (table.firstElementChild.localName == "tei-head") {
-        let head = table.firstElementChild;
-        head.remove();
-        let caption = document.createElement("caption");
-        caption.innerHTML = head.innerHTML;
-        table.appendChild(caption);
-      }
-      for (let row of Array.from(table.querySelectorAll("tei-row"))) {
-        let tr = document.createElement("tr");
-        tr.innerHTML = row.innerHTML;
-        for (let attr of Array.from(row.attributes)) {
-          tr.setAttribute(attr.name, attr.value);
+      if (elt.firstElementChild.localName != "table") {
+        let table = document.createElement("table");
+        table.innerHTML = elt.innerHTML;
+        if (table.firstElementChild.localName == "tei-head") {
+          let head = table.firstElementChild;
+          head.remove();
+          let caption = document.createElement("caption");
+          caption.innerHTML = head.innerHTML;
+          table.appendChild(caption);
         }
-        row.parentElement.replaceChild(tr, row);
-      }
-      for (let cell of Array.from(table.querySelectorAll("tei-cell"))) {
-        let td = document.createElement("td");
-        if (cell.hasAttribute("cols")) {
-          td.setAttribute("colspan", cell.getAttribute("cols"));
+        for (let row of Array.from(table.querySelectorAll("tei-row"))) {
+          let tr = document.createElement("tr");
+          tr.innerHTML = row.innerHTML;
+          for (let attr of Array.from(row.attributes)) {
+            tr.setAttribute(attr.name, attr.value);
+          }
+          row.parentElement.replaceChild(tr, row);
         }
-        td.innerHTML = cell.innerHTML;
-        for (let attr of Array.from(cell.attributes)) {
-          td.setAttribute(attr.name, attr.value);
+        for (let cell of Array.from(table.querySelectorAll("tei-cell"))) {
+          let td = document.createElement("td");
+          if (cell.hasAttribute("cols")) {
+            td.setAttribute("colspan", cell.getAttribute("cols"));
+          }
+          td.innerHTML = cell.innerHTML;
+          for (let attr of Array.from(cell.attributes)) {
+            td.setAttribute(attr.name, attr.value);
+          }
+          cell.parentElement.replaceChild(td, cell);
         }
-        cell.parentElement.replaceChild(td, cell);
+        this.hideContent(elt, true);
+        elt.appendChild(table);
       }
-      this.hideContent(elt, true);
-      elt.appendChild(table);
     }
   }
 }
